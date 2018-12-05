d51d7f98f267334058819a21fa4440eca30de258
Sidinei Silva <sidinei.silva02@gmail.com>
2018-12-05 14:36:04 -0300

    Ativando o trackable para armazenar dados do ultimo login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, and :omniauthable, :recoverable :rememberable,
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :recoverable :rememberable,
   devise :database_authenticatable, :registerable, :validatable, :trackable
 end
