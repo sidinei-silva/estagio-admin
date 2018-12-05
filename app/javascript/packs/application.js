c4e3b63cfab54c6799e89457f7e1dc782d272ada
Sidinei Silva <sidinei.silva02@gmail.com>
2018-12-05 20:30:29 -0300

    Importando vuetify no application

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './app.vue'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 document.addEventListener('DOMContentLoaded', () => {
     const app = new Vue({
-        el: '#app' ,
+        el: '#app',
         render: h => h(App)
     })
 })
-
