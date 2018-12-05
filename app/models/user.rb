76c6143950d16b62296aadfb00ff7d347cc802c1
Sidinei Silva <sidinei.silva02@gmail.com>
2018-12-05 14:13:07 -0300

    Editando model user para escolher modulos devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, and :omniauthable, :recoverable :rememberable,
+  devise :database_authenticatable, :registerable, :validatable, :trackable
 end
