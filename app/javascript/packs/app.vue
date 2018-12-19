d36aa1b1e78f9c9db5eea00d1ca0c9d7a59670f9
Sidinei Silva <sidinei.silva02@gmail.com>
2018-12-19 20:56:59 -0300

    Separando header em componente

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,22 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" fixed app>
-      <v-list v-for="item in items_sidebar" dense>
-        <v-list-tile @click=""  :to="{path: item.href}" :key="index">
-          <v-list-tile-action>
-            <v-icon>{{item.icon}}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{item.title}}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar color="indigo" dark fixed app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Estagio Admin</v-toolbar-title>
-    </v-toolbar>
+    <Header/>
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -27,6 +11,7 @@
 <script>
 
     import Footer from './components/shared/footer';
+    import Header from './components/shared/header'
 
     export default {
         data: () => ({
@@ -48,7 +33,8 @@
             source: String
         },
         components: {
-            'Footer': Footer
+            'Footer': Footer,
+            'Header': Header
         }
     }
 </script>
---
+++ app/javascript/packs/components/shared/header.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-header>
+        <v-navigation-drawer v-model="drawer" fixed app>
+            <v-list v-for="item in items_sidebar" dense>
+                <v-list-tile @click=""  :to="{path: item.href}" :key="index">
+                    <v-list-tile-action>
+                        <v-icon>{{item.icon}}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{item.title}}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar color="indigo" dark fixed app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>Estagio Admin</v-toolbar-title>
+        </v-toolbar>
+    </v-header>
+</template>
+<script>
+    export default {
+        data: () => ({
+            drawer: null,
+            items_sidebar: [{
+                href: '/',
+                router: true,
+                title: 'Home',
+                icon: 'home',
+            }, {
+                href: 'estagiarios',
+                router: true,
+                title: 'Estagiarios',
+                icon: 'extension',
+            }],
+        }),
+        props: {
+            source: String
+        }
+    }
+</script>
